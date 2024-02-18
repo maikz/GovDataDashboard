f1e7e40b70c184f0c08ceb078e8e19688570cb9f
Maik Zarnbach <maikzarnbach@gmail.com>
2024-02-18 21:06:04 +0100

    Display fetches data in HTML table in root.

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,14 @@
 package com.example.plugins
 
+import com.example.GovDataFetcher
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.statuspages.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
 
@@ -17,8 +20,37 @@ fun Application.configureRouting() {
 
     routing {
         staticResources("/content", "mycontent")
+
         get("/") {
-            call.respondText("Hello World!")
+            val organisations = GovDataFetcher().fetch()
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    title {
+                        +"Departments by dataset count"
+                    }
+                }
+                body {
+                    h1 {
+                        +"Departments by dataset count"
+                    }
+                    table {
+                        tr {
+                            th { +"Name" }
+                            th { +"Number of datasets" }
+                        }
+                        for (organisation in organisations) {
+                            tr {
+                                td {
+                                    +organisation.display_name
+                                }
+                                td {
+                                    +"${organisation.package_count}"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         get("/test1") {
